bb7000b5126fc9c1449532ca34def482234a19ce
robchendev <robchendev@gmail.com>
2023-06-10 18:03:02 -0700

    Initial Commit

---
+++ learn-swift/learn_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_swiftApp.swift
+//  learn-swift
+//
+//  Created by Robert Chen on 2023-06-10.
+//
+
+import SwiftUI
+
+@main
+struct learn_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
